329172aed6fbc9ea618e4f82a41c394a5fc91d82
NathanLee2021 <818998@mystma.org>
2021-03-12 02:37:55 +0000

    test try #1

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-#WRITE YOUR CODE IN THIS FILE
+def times10(n):
+    return n * 10
+
+def firstA(x):
+    if x [0] == "a":
+        return "True"
+    else:
+        return "False"
+
+def sizer(s):
+    if s < 10:
+        return "small"
+    elif 10 < s > 100:
+            return "medium"
+    else:
+        return "large"
+
+print (sizer("12"))
